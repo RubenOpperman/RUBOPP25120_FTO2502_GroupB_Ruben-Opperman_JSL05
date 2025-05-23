a410ca60d52a1c366aa3336e26fa86e192fabae8
RubenOpperman <Oppiesfam123@gmail.com>
2025-05-23 12:22:00 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Kanban Board UI Project
+
+## 📋 Project Overview
+
+This project is a fully responsive user interface built based on a Figma design. The main objective was to accurately recreate the layout using HTML and CSS, with a strong focus on clean code structure and responsive styling.
+
+JavaScript has been added to introduce interactivity. The application loads tasks from an array of objects and displays them within their respective columns. When a task is clicked, a modal appears, giving the impression that the task can be edited—however, the edit functionality is not yet implemented.
+
+## 🎨 Figma Design Link
+
+[View the Figma Design](https://www.figma.com/design/y7bFCUYL5ZHfPeojACBXg2/Challenges-%7C-JSL?node-id=0-1&p=f)
+
+## 🛠️ Technologies Used
+
+- **HTML5** – Structure and content
+- **CSS3** / **TailwindCSS** – Styling and responsiveness
+- **Figma** – UI design reference
+- **JavaScript** – Adds interactivity and dynamic behavior
+
+## 🚀 How to Run Locally
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/RubenOpperman/RUBOPP25120_FTO2502_GroupB_Ruben-Opperman_JSL04.git
+
+```
+
+## 2. Navigate into the project directory
+
+```bash
+cd RUBOPP25120_FTO2502_GroupB_Ruben-Opperman_JSL04
+```
+
+## 3. Install dependencies
+
+```bash
+npm install
+```
+
+## 4. Start the development server
+
+```bash
+npm run dev
+```
---
+++ src/main.js
@@ -0,0 +1,170 @@
+// array of objects containing tasks
+const initialTasks = [
+  {
+    id: 1,
+    title: "Launch Epic Career 🚀",
+    description: "Create a killer Resume",
+    status: "todo",
+  },
+  {
+    id: 2,
+    title: "Master JavaScript 💛",
+    description: "Get comfortable with the fundamentals",
+    status: "doing",
+  },
+  {
+    id: 3,
+    title: "Keep on Going 🏆",
+    description: "You're almost there",
+    status: "doing",
+  },
+  {
+    id: 4,
+    title: "Learn Data Structures and Algorithms 📚",
+    description:
+      "Study fundamental data structures and algorithms to solve coding problems efficiently",
+    status: "todo",
+  },
+  {
+    id: 5,
+    title: "Contribute to Open Source Projects 🌐",
+    description:
+      "Gain practical experience and collaborate with others in the software development community",
+    status: "done",
+  },
+  {
+    id: 6,
+    title: "Build Portfolio Projects 🛠️",
+    description:
+      "Create a portfolio showcasing your skills and projects to potential employers",
+    status: "done",
+  },
+];
+
+const todoContainer = document.getElementById("todo-container");
+const doingContainer = document.getElementById("doing-container");
+const doneContainer = document.getElementById("done-container");
+const modal = document.getElementById("edit-modal");
+const titleInput = document.getElementById("modal-title");
+const descriptionInput = document.getElementById("modal-description");
+const statusSelect = document.getElementById("modal-status");
+const closeModalBtn = document.getElementById("close-modal");
+
+let selectedTask = null;
+
+/**
+ * This function activates when a task is clicked and then retrieves the task clicked and opens a modal.
+ *
+ * @param {object} tasks - id, title, description and status.
+ */
+function openModal(task) {
+  selectedTask = task;
+
+  titleInput.value = task.title;
+  descriptionInput.value = task.description;
+  statusSelect.value = task.status;
+
+  modal.showModal();
+}
+
+/**
+ * This function closes the modal.
+ */
+function closeModal() {
+  modal.close();
+  selectedTask = null;
+  titleInput.value = "";
+  descriptionInput.value = "";
+  statusSelect.value = "";
+}
+/**
+ * This function takes in an array and sorts the tasks with the status todo and displays them dynamically.
+ * @param {array} tasks -array of objects.
+ */
+function loadTodoTasks(tasks) {
+  tasks.forEach((task) => {
+    if (task.status === "todo") {
+      const todoTask = document.createElement("div");
+      todoTask.innerText = task.title;
+
+      todoTask.classList.add(
+        "bg-primary-color",
+        "text-primary-font-color",
+        "leading-[19px]",
+        "text-sm",
+        "font-bold",
+        "mb-5",
+        "py-5",
+        "pl-5",
+        "rounded-md",
+        "shadow-lg"
+      );
+      todoTask.addEventListener("click", () => openModal(task));
+
+      todoContainer.appendChild(todoTask);
+    }
+  });
+}
+/**
+ * This function takes in an array and sorts the tasks with the status doing and displays them dynamically.
+ * @param {array} tasks -array of objects.
+ */
+function loadDoingTasks(tasks) {
+  tasks.forEach((task) => {
+    if (task.status === "doing") {
+      const doingTask = document.createElement("div");
+      doingTask.innerText = task.title;
+
+      doingTask.classList.add(
+        "bg-primary-color",
+        "text-primary-font-color",
+        "leading-[19px]",
+        "text-sm",
+        "font-bold",
+        "mb-5",
+        "py-5",
+        "pl-5",
+        "rounded-md",
+        "shadow-lg"
+      );
+      doingTask.addEventListener("click", () => openModal(task));
+
+      doingContainer.appendChild(doingTask);
+    }
+  });
+}
+/**
+ * This function takes in an array and sorts the tasks with the status done and displays them dynamically.
+ * @param {array} tasks -array of objects.
+ *
+ */
+function loadDoneTasks(tasks) {
+  tasks.forEach((task) => {
+    if (task.status === "done") {
+      const doneTask = document.createElement("div");
+      doneTask.innerText = task.title;
+
+      doneTask.classList.add(
+        "bg-primary-color",
+        "text-primary-font-color",
+        "leading-[19px]",
+        "text-sm",
+        "font-bold",
+        "mb-5",
+        "py-5",
+        "pl-5",
+        "rounded-md",
+        "shadow-lg"
+      );
+      doneTask.addEventListener("click", () => openModal(task));
+
+      doneContainer.appendChild(doneTask);
+    }
+  });
+}
+
+closeModalBtn.addEventListener("click", closeModal);
+
+loadTodoTasks(initialTasks);
+loadDoingTasks(initialTasks);
+loadDoneTasks(initialTasks);
